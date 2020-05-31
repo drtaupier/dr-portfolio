8fb9e093662a432156ef3ddd02f3b734e08b10fb
drtaupier <drtaupier@gmail.com>
2020-05-30 22:08:40 -0400

    I did the footer

--- src/client/js/classes.js
+++ src/client/js/classes.js
@@ -2,7 +2,7 @@ export class UI {
     footer() {
         const today = new Date();
         const year = today.getFullYear();
-        const footer = document.getElementById(footer);
+        const footer = document.getElementById('footer');
         footer.innerHTML = `<h3>David Rivera Taupier</h3>`;
         footer.innerHTML += `<h2>Copyright &copy; ${year}</h2>`;
     }
